57ef20285e5cc3a7c1846c9b4687f1739a580dcc
Marat Radchenko <marat@slonopotamus.org>
2025-06-01 18:16:56 +0300

    Reformat code

--- Source/AttoClient/Private/AttoClient.cpp
+++ Source/AttoClient/Private/AttoClient.cpp
@@ -4,10 +4,12 @@
 #include "WebSocketsModule.h"
 
 FAttoClient::FAttoClient(const FString& Url)
-	: WebSocket{FWebSocketsModule::Get().CreateWebSocket(Url, Atto::Protocol)}
+    : WebSocket{FWebSocketsModule::Get().CreateWebSocket(Url, Atto::Protocol)}
 {
-	WebSocket->OnConnected().AddLambda([&]{ OnConnected.Broadcast(); });
-	WebSocket->OnClosed().AddLambda([&](int32 StatusCode, const FString& Reason, bool bWasClean){ OnDisconnected.Broadcast(Reason, bWasClean); });
+	WebSocket->OnConnected().AddLambda([&]
+	                                   { OnConnected.Broadcast(); });
+	WebSocket->OnClosed().AddLambda([&](int32 StatusCode, const FString& Reason, bool bWasClean)
+	                                { OnDisconnected.Broadcast(Reason, bWasClean); });
 }
 
 void FAttoClient::Connect()
