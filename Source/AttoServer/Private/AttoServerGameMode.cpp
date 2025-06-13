c3ec5f876e4ae406b1cf724032cb242d0a0c42c0
Marat Radchenko <marat@slonopotamus.org>
2025-06-13 19:13:48 +0300

    Remove bogus comment

--- Source/AttoServer/Private/AttoServerGameMode.cpp
+++ Source/AttoServer/Private/AttoServerGameMode.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "AttoServerGameMode.h"
 
 void AAttoServerGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
